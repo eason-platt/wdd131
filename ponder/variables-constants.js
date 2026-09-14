0f78651d88538b5efc858cae611ba5106e5b8590
Eason Platt <eplatt@byui.edu>
2026-09-13 20:59:14 -0600

    Ponder JS Variables

---
+++ ponder/variables-constants.js
@@ -0,0 +1,32 @@
+const PI = Math.PI;
+let radius = 3;
+
+let area = Math.sqrt(radius) * PI;
+
+console.log(area);
+
+radius = 20;
+area = Math.sqrt(radius) * PI;
+console.log(area);
+
+const circleArea = radius => Math.sqrt(radius) * Math.PI;
+
+console.log(circleArea(20));
+
+
+const one = 1;
+const two = '2';
+
+console.log(one * two);
+console.log(one + two);
+console.log(one + Number(two));
+
+
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "John";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+console.log(student); //does not work, can't access a block variable outside the block
